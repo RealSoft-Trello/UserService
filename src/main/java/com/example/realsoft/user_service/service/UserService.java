89ac3819d85b8ac9bd1deab6cdced8f8061612ff
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2023-05-18 12:41:58 -0400

    removed create user and login user endpoints and moved them to AuthService

--- src/main/java/com/example/realsoft/user_service/service/UserService.java
+++ src/main/java/com/example/realsoft/user_service/service/UserService.java
@@ -1,19 +1,14 @@
 package com.example.realsoft.user_service.service;
 
-import com.example.realsoft.user_service.entity.Role;
 import com.example.realsoft.user_service.exception.ResourceNotFound;
-import com.example.realsoft.user_service.model.UserLogin;
 import com.example.realsoft.user_service.model.UserRequest;
 import com.example.realsoft.user_service.model.UserResponse;
 
 import java.util.List;
 
 public interface UserService {
-    UserResponse getUserByEmail(UserLogin userLogin) throws ResourceNotFound;
-    UserResponse createUser(UserRequest userRequest) throws ResourceNotFound;
     UserResponse getUserById(Long userId) throws ResourceNotFound;
     void deleteUser(Long userId) throws ResourceNotFound;
     UserResponse updateUser(Long userId, UserRequest userRequest) throws ResourceNotFound;
     List<UserResponse> getAllUsers();
-    Role saveRole(Role role);
 }
