3494ffbdfec492a81287e1198beb9eda7aa57640
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2023-05-18 12:43:50 -0400

    fixed user controller

---
+++ src/main/java/com/example/realsoft/user_service/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.realsoft.user_service.controller;
+
+import com.example.realsoft.user_service.exception.ResourceNotFound;
+import com.example.realsoft.user_service.model.UserRequest;
+import com.example.realsoft.user_service.model.UserResponse;
+import com.example.realsoft.user_service.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/realsoft/trello/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable(name = "id") Long userId) throws ResourceNotFound {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable(name = "id") Long userId, @Valid @RequestBody UserRequest userRequest) throws ResourceNotFound {
+        return ResponseEntity.ok(userService.updateUser(userId, userRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable(name = "id") Long userId) throws ResourceNotFound {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok("User deleted successfully!");
+    }
+}
